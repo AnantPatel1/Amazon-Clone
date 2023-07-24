5b37c4d6ad0a7e67b44c66ee3c81d25fe4713f14
Anant <anantpatel07012003@gmail.com>
2023-07-25 04:30:08 +0530

    jwelery section updated

--- src/Components/Product/AvailableProduct.js
+++ src/Components/Product/AvailableProduct.js
@@ -4,53 +4,45 @@ import { useProducts } from "../../Store/ProductContext";
 import { Fragment } from "react";
 export default function AvailableProduct() {
   const product = useProducts();
+
   const mainItem = product.filter((item) => item.id < 6);
   return (
     <Fragment>
       <div className={styles.home__row}>
-        {mainItem
-          .filter((item) => item.id % 6 === 0 || item.id % 6 === 1)
-          .map((item) => (
-            <Product
-              key={item.id}
-              id={item.id}
-              title={item.title}
-              price={item.price}
-              rating={item.rating}
-              image={item.image}
-            />
-          ))}
+        {mainItem.slice(0, 2).map((item) => (
+          <Product
+            key={item.id}
+            id={item.id}
+            title={item.title}
+            price={item.price}
+            rating={item.rating}
+            image={item.image}
+          />
+        ))}
       </div>
       <div className={styles.home__row}>
-        {mainItem
-          .filter(
-            (item) =>
-              item.id % 6 === 2 || item.id % 6 === 3 || item.id % 6 === 4
-          )
-          .map((item) => (
-            <Product
-              key={item.id}
-              id={item.id}
-              title={item.title}
-              price={item.price}
-              rating={item.rating}
-              image={item.image}
-            />
-          ))}
+        {mainItem.slice(2, 5).map((item) => (
+          <Product
+            key={item.id}
+            id={item.id}
+            title={item.title}
+            price={item.price}
+            rating={item.rating}
+            image={item.image}
+          />
+        ))}
       </div>
       <div className={styles.home__row}>
-        {mainItem
-          .filter((item) => item.id % 6 === 5)
-          .map((item) => (
-            <Product
-              key={item.id}
-              id={item.id}
-              title={item.title}
-              price={item.price}
-              rating={item.rating}
-              image={item.image}
-            />
-          ))}
+        {mainItem.slice(5, 6).map((item) => (
+          <Product
+            key={item.id}
+            id={item.id}
+            title={item.title}
+            price={item.price}
+            rating={item.rating}
+            image={item.image}
+          />
+        ))}
       </div>
     </Fragment>
   );
--- src/Components/Product/ItemTypes/Jwelery.js
+++ src/Components/Product/ItemTypes/Jwelery.js
@@ -1,61 +1,36 @@
 import React from "react";
 import Product from "../Product";
-import { Fragment } from "react";
 import styles from "../AvailableProduct.module.css";
 import { useProducts } from "../../../Store/ProductContext";
+
 export default function Jwelery({ products }) {
   const product = useProducts();
   const jwelery = product.filter(
     (item) => item.type === "jwelery" && item.id > 5
   );
   console.log(jwelery);
-  return (
-    <Fragment>
-      <div className={styles.home__row}>
-        {jwelery
-          .filter((item) => item.id % 6 === 0 || item.id % 6 === 1)
-          .map((item) => (
-            <Product
-              key={item.id}
-              id={item.id}
-              title={item.title}
-              price={item.price}
-              rating={item.rating}
-              image={item.image}
-            />
-          ))}
+
+  const productDivs = [];
+
+  for (let i = 0; i < jwelery.length; i += 3) {
+    const productsSlice = jwelery.slice(i, i + 3);
+    const productItems = productsSlice.map((item) => (
+      <Product
+        key={item.id}
+        id={item.id}
+        title={item.title}
+        price={item.price}
+        rating={item.rating}
+        image={item.image}
+      />
+    ));
+
+    productDivs.push(
+      <div className={styles.home__row} key={i}>
+        {productItems}
       </div>
-      <div className={styles.home__row}>
-        {jwelery
-          .filter(
-            (item) =>
-              item.id % 6 === 2 || item.id % 6 === 3 || item.id % 6 === 4
-          )
-          .map((item) => (
-            <Product
-              key={item.id}
-              id={item.id}
-              title={item.title}
-              price={item.price}
-              rating={item.rating}
-              image={item.image}
-            />
-          ))}
-      </div>
-      <div className={styles.home__row}>
-        {jwelery
-          .filter((item) => item.id % 6 === 5)
-          .map((item) => (
-            <Product
-              key={item.id}
-              id={item.id}
-              title={item.title}
-              price={item.price}
-              rating={item.rating}
-              image={item.image}
-            />
-          ))}
-      </div>
-    </Fragment>
-  );
+    );
+  }
+
+  return <>{productDivs}</>;
 }
